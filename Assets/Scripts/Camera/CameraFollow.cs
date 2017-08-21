78a7ffe314aa382865f5a4e564a5351c8c8f88e0
Neo <neo.li@jam3.com>
2017-08-21 17:45:39 -0400

    Camera setup

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform Target;
+	public float Damp = 5f;
+
+	Vector3 offset;
+
+	void Start() {
+		offset = transform.position - Target.position;
+	}
+
+	void FixedUpdate() {
+		Vector3 CameraTargetPosition = Target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, CameraTargetPosition, Damp * Time.deltaTime);
+	}
+}
