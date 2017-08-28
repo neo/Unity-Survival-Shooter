60ff61c5b46f022dcbfc507e58ac6dee67c25336
Neo <neo.li@jam3.com>
2017-08-28 17:46:06 -0400

    First ARKit attempt

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.iOS;
 
 public class GameManager : MonoBehaviour {
 
 	public GameObject Game;
 
 	GameObject Clone;
+	Vector3 position;
+	Quaternion rotation;
 
 	void Start () {
-		Clone = Instantiate (Game, transform);
+//		Clone = Instantiate (Game, transform);
+		UnityARSessionNativeInterface.ARAnchorAddedEvent += AddAnchor;
+	}
+
+	public void AddAnchor(ARPlaneAnchor arPlaneAnchor) {
+		if (!Clone) {
+			position = UnityARMatrixOps.GetPosition (arPlaneAnchor.transform);
+			rotation = UnityARMatrixOps.GetRotation (arPlaneAnchor.transform);
+			Clone = Instantiate (Game, position, rotation, transform);
+		}
 	}
 
 	public void Restart () {
 		Destroy (Clone);
-		Clone = Instantiate (Game, transform);
+		Clone = Instantiate (Game, position, rotation, transform);
 	}
 }
